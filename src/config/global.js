d25ace7b439977ee1dee4b0258bcb75aad7c6270
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2023-07-26 10:25:42 -0500

    Ajuste CFa

--- src/config/global.js
+++ src/config/global.js
@@ -128,7 +128,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/921200_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
